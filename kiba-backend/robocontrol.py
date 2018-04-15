57b278f4cc2c0278d81eef3259c34695c2f2fd2e
Kai Wieland <post@kai.gs>
2018-04-15 20:52:25 +0200

    RoboTest graduated to RoboControl, handles percentage now

--- kiba-backend/robocontrol.py
+++ kiba-backend/robocontrol.py
@@ -1,104 +1,60 @@
-import RPi.GPIO as GPIO, time, os, subprocess
-#import testGPIO as GPIO
-#import time
+import time
+from gpiozero import Button, LED, OutputDevice
 
-# Raspberry GPIO setup
-GPIO.setmode(GPIO.BCM)
+led_01 = LED(22)
+led_02 = LED(26)
+led_03 = LED(5)
+led_04 = LED(6)
 
-RESET = 12
-CUP_LIGHT = 25
-LED_01 = 22
-LED_02 = 26
-LED_03 = 5
-LED_04 = 6
-LED_05 = 12
+status_leds = [led_01, led_02, led_03, led_04]
 
-BUTTON_LED = 23
-BUTTON = 24
+cup_light = LED(25)
 
-REL_01 = 17
-REL_02 = 4
-#REL_03 = 27
-#REL_04 = 18
+button_led = LED(23)
+button = Button(24)
+button_reset = Button(12)
+
+relais_01 = OutputDevice(17)
+relais_02 = OutputDevice(4)
 
 class RoboControl:
-	def __init__(self):
-		GPIO.setup(RESET, GPIO.IN)
-		GPIO.setup(CUP_LIGHT, GPIO.OUT)
+	def reset(self):
+		button_led.off()
+		relais_01.off()
+		relais_02.off()
+		cup_light.off()
+		self.status_leds_off()
+		print "RoboControl reset complete"
 
-		GPIO.setup(LED_01, GPIO.OUT)
-		GPIO.setup(LED_02, GPIO.OUT)
-		GPIO.setup(LED_03, GPIO.OUT)
-		GPIO.setup(LED_04, GPIO.OUT)
-		GPIO.setup(LED_05, GPIO.OUT)
+	def status_leds_on(self):
+		for led in status_leds:
+			led.on()
 
-		GPIO.setup(BUTTON_LED, GPIO.OUT)
-		GPIO.setup(BUTTON, GPIO.IN)
+	def status_leds_off(self):
+		for led in status_leds:
+			led.off()
 
-		GPIO.setup(REL_01, GPIO.OUT)
-		GPIO.setup(REL_02, GPIO.OUT)
-		#GPIO.setup(REL_03, GPIO.OUT)
-		#GPIO.setup(REL_04, GPIO.OUT)
+	def makeKIBA(self, percentage):
+		print "KIBA making starts now with percentage", percentage
+		button_led.on()
+		led_01.on()
+		relais_01.on()
+		time.sleep(3.8/100*percentage)
+		relais_01.off()
+		led_02.on()
+		relais_02.on()
+		time.sleep(1)
+		relais_02.off()
+		led_03.on()
+		print "KIBA poured"
+		time.sleep(3.8-3.8/100*percentage)
+		cup_light.blink()
+		time.sleep(5)
+		cup_light.off()
+		button_led.off()
+		self.status_leds_off()
+		print "KIBA process finished"
 
-		self.reset
-
-	def reset():
-	  GPIO.output(CUP_LIGHT, False)
-	  GPIO.output(BUTTON_LED, False)
-	  GPIO.output(REL_01, False)
-	  GPIO.output(REL_02, False)
-
-	  GPIO.output(LED_01, False)
-	  GPIO.output(LED_02, False)
-	  GPIO.output(LED_03, False)
-	  GPIO.output(LED_04, False)
-	  GPIO.output(LED_05, False)
-	  print "RoboControl reset complete"
-
-	def mixloop(self, percentage):
-		while True:
-			# TODO: aktivierung durch event (die website)
-			while not GPIO.input(BUTTON):
-				GPIO.output(BUTTON_LED, True)
-				time.sleep(0.1)
-				GPIO.output(BUTTON_LED, False)
-				time.sleep(0.1)
-
-			# TODO: variable x kommt von website, variable y ist feste Zeit (z.b 3.8) - x
-			if (GPIO.input(BUTTON)):
-				print("drinks stirred")
-				GPIO.output(BUTTON_LED, True)
-				GPIO.output(LED_01, True)
-				time.sleep(0.2)
-				GPIO.output(LED_02, True)
-				GPIO.output(REL_01, True)
-				# time.sleep(x)
-				GPIO.output(LED_03, True)
-				GPIO.output(REL_01, False)
-				GPIO.output(REL_02, True)
-				# time.sleep(y)
-				GPIO.output(LED_04, True)
-				GPIO.output(REL_02, False)
-				print("drinks delivered")
-				time.sleep(0.2)
-				GPIO.output(LED_05, True)
-
-				for i in range(5):
-					GPIO.output(CUP_LIGHT, True)
-					time.sleep(0.2)
-					GPIO.output(CUP_LIGHT, False)
-					time.sleep(0.2)
-
-				GPIO.output(BUTTON_LED, False)
-				GPIO.output(CUP_LIGHT, True)
-				time.sleep(5)
-
-				GPIO.output(CUP_LIGHT, False)
-
-				GPIO.output(LED_01, False)
-				GPIO.output(LED_02, False)
-				GPIO.output(LED_03, False)
-				GPIO.output(LED_04, False)
-				GPIO.output(LED_05, False)
-				time.sleep(0.1)
-				print("drinks ready")
+	def ready(self, percentage):
+		button.when_released = button_led.blink()
+		button.when_pressed = lambda : self.makeKIBA(percentage)
