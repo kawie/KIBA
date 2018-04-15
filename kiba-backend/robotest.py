f271bcadfc41d90e01aedb5e0a0167790fc75c53
Kai Wieland <post@kai.gs>
2018-04-15 16:50:46 +0200

    RoboTest with button

--- kiba-backend/robotest.py
+++ kiba-backend/robotest.py
@@ -2,19 +2,27 @@ import RPi.GPIO as GPIO, time, os, subprocess
 
 GPIO.setmode(GPIO.BCM)
 
-LED_01 = 23
+# LED_01 = 23
+BUTTON_LED = 23
+BUTTON = 24
 
 class RoboControl:
 	def __init__(self):
-		GPIO.setup(LED_01, GPIO.OUT)
+		GPIO.setup(BUTTON_LED, GPIO.OUT)
+		GPIO.setup(BUTTON, GPIO.IN)
 		self.reset()
 
 	def reset(self):
-	  GPIO.output(LED_01, False)
+		GPIO.output(BUTTON_LED, False)
 
 	def light(self, timer):
-  		for i in range(timer):
-  			GPIO.output(LED_01, True)
-			time.sleep(0.5)
-			GPIO.output(LED_01, False)
-			time.sleep(0.5)
+		while not GPIO.input(BUTTON):
+				GPIO.output(BUTTON_LED, True)
+				time.sleep(0.1)
+				GPIO.output(BUTTON_LED, False)
+				time.sleep(0.1)
+		if (GPIO.input(BUTTON)):
+	  		for i in range(timer):
+	  			GPIO.output(LED_01, True)
+				time.sleep(20)
+				GPIO.output(LED_01, False)
