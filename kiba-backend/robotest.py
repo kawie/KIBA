2eaddb443463a46ea2de7d8abb33fc85e4d93bef
Pi Kiba <pi@kiba.fun>
2018-04-15 14:14:54 +0000

    fixes to RoboTest #

--- kiba-backend/robotest.py
+++ kiba-backend/robotest.py
@@ -6,16 +6,14 @@ LED_01 = 23
 
 class RoboControl:
 	def __init__(self):
-		GPIO.setup(RESET, GPIO.IN)
-		GPIO.setup(CUP_LIGHT, GPIO.OUT)
 		GPIO.setup(LED_01, GPIO.OUT)
 		self.reset
 
 	def reset():
 	  GPIO.output(LED_01, False)
 
-	def light(self, timer: int):
-  		while(true):
+	def light(self, timer):
+  		while True:
   			GPIO.output(LED_01, True)
 			time.sleep(timer)
 			GPIO.output(LED_01, False)
