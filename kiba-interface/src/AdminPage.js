c6ae4131df4b3aa6e6d71bc0b4a37907e64c3486
Kai Wieland <post@kai.gs>
2018-08-21 15:16:56 +0200

    Adds toggleable AdminPage

--- kiba-interface/src/AdminPage.js
+++ kiba-interface/src/AdminPage.js
@@ -3,15 +3,39 @@ import { Field } from 'formik';
 import axios from 'axios';
 
 class AdminPage extends Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			adminOpen: false
+		}
+	}
+
+	toggleAdmin() {
+		this.setState({
+		  adminOpen: !this.state.adminOpen
+		})
+	}
+
 	render() {
 		return (
-			<div>
+		<div>
+			<div style={{margin: '50px auto', opacity: '0'}}>
+			  <label>
+			    <input type="checkbox" onClick={ () => this.toggleAdmin()} />
+			    <span className="checkmark"></span>
+			  </label>
+			</div>
+
+			{this.state.adminOpen &&
+			<div className="captchaBox">
 				<Field name="serverIP" />
 				<button type="button" onClick={() => { axios.get('http://' + this.props.values.serverIP + ':5000/kiba/reset') }}>Reset</button>
 				<button type="button" onClick={() => { axios.get('http://' + this.props.values.serverIP + ':5000/kiba/clean') }}>Clean</button><br />
 				<button type="button" onClick={() => { axios.get('http://' + this.props.values.serverIP + ':5000/kiba/lights/on') }}>Lights on</button>
 				<button type="button" onClick={() => { axios.get('http://' + this.props.values.serverIP + ':5000/kiba/lights/off') }}>Lights off</button>
 			</div>
+			}
+		</div>
 		)
 	}
 }
